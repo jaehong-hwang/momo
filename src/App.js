da8bf4ad05a6b63de3042956069631713f9c7320
Jaehong-Hwang <krusnocide@gmail.com>
2018-09-15 01:36:47 +0900

    통계 확인 텍스트 추가

--- src/App.js
+++ src/App.js
@@ -7,14 +7,16 @@
  */
 
 import React, {Component} from 'react'
-import {StyleSheet, View, Text, Dimensions} from 'react-native'
+import {StyleSheet, View, Text, Image, Dimensions} from 'react-native'
 import Expense from './components/Expense'
 import AppHeader from './components/Header'
 
 export default class App extends Component {
   render() {
+    let {height, width} = Dimensions.get('window')
+
     return (
-      <View style={styles.absolute}>
+      <View style={{height: height}}>
         <AppHeader style={styles.header} />
         <View style={styles.body}>
           <Expense title="이번달 지출" price="312540" style={styles.expense} />
@@ -24,6 +26,13 @@ export default class App extends Component {
             통계를 확인하여 어떤 예상치 못한 지출이{"\n"}
             있었는지 확인 해 보세요!
           </Text>
+        </View>
+        <View style={{position: 'absolute', alignSelf: 'center', bottom: 100}}>
+          <Text style={styles.swypeText}>아래로 넘겨서 통계 확인</Text>
+          <Image
+            style={{width: 17, height: 17, alignSelf: 'center', marginTop: 5}}
+            source={require('./assets/images/arrow-2bottom.png')}
+          />
         </View>
       </View>
     )
@@ -42,5 +51,9 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: 'rgb(94, 94, 94)',
     lineHeight: 22,
-  }
+  },
+  swypeText: {
+    fontSize: 16,
+    color: 'rgb(94, 94, 94)',
+  },
 })
